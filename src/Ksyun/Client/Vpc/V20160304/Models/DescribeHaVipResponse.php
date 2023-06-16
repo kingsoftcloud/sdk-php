d771279e8d2970ffb61b66e8858b2fa5b13e23df
FengYikai <feng_yikai@foxmail.com>
2023-06-16 17:02:19 +0800

    Update SDK

---
+++ src/Ksyun/Client/Vpc/V20160304/Models/DescribeHaVipResponse.php
@@ -0,0 +1,38 @@
+<?php
+namespace  Ksyun\Client\Vpc\V20160304\Models;
+
+use Ksyun\Common\BaseModel;
+
+class DescribeHaVipResponse extends BaseModel
+{
+         /** 请求ID**/
+         public  $RequestId;
+
+         /** 获取另一页返回结果的 token.**/
+         public  $NextToken;
+
+         /**Object 高可用虚拟IP的信息**/
+         public  $HaVipSet;
+
+         public function __construct()
+         {
+
+         }
+
+        public function unserialize($param)
+        {
+            if ($param === null) {
+                return;
+            }
+            if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
+                $this->RequestId = $param["RequestId"];
+            }
+            if (array_key_exists("NextToken",$param) and $param["NextToken"] !== null) {
+                $this->NextToken = $param["NextToken"];
+            }
+            if (array_key_exists("HaVipSet",$param) and $param["HaVipSet"] !== null) {
+                $this->HaVipSet = $param["HaVipSet"];
+            }
+
+        }
+}
