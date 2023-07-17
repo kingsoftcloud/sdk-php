4f337bf2f8f963662081d906563c15e8b5ee4202
FengYikai <feng_yikai@foxmail.com>
2023-07-17 14:40:09 +0800

    Update SDK

---
+++ src/Ksyun/Client/Kls/V20170101/Models/CheckBlacklistResponse.php
@@ -0,0 +1,32 @@
+<?php
+namespace  Ksyun\Client\Kls\V20170101\Models;
+
+use Ksyun\Common\BaseModel;
+
+class CheckBlacklistResponse extends BaseModel
+{
+         /** **/
+         public  $Data;
+
+         /** **/
+         public  $RequestId;
+
+         public function __construct()
+         {
+
+         }
+
+        public function unserialize($param)
+        {
+            if ($param === null) {
+                return;
+            }
+            if (array_key_exists("Data",$param) and $param["Data"] !== null) {
+                $this->Data = $param["Data"];
+            }
+            if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
+                $this->RequestId = $param["RequestId"];
+            }
+
+        }
+}
---
+++ src/Ksyun/Client/Kls/V20170101/Models/ListRealtimeStreamsInfoResponse.php
@@ -0,0 +1,26 @@
+<?php
+namespace  Ksyun\Client\Kls\V20170101\Models;
+
+use Ksyun\Common\BaseModel;
+
+class ListRealtimeStreamsInfoResponse extends BaseModel
+{
+         /** **/
+         public  $ListRealtimeStreamsInfoResponse;
+
+         public function __construct()
+         {
+
+         }
+
+        public function unserialize($param)
+        {
+            if ($param === null) {
+                return;
+            }
+            if (array_key_exists("ListRealtimeStreamsInfoResponse",$param) and $param["ListRealtimeStreamsInfoResponse"] !== null) {
+                $this->ListRealtimeStreamsInfoResponse = $param["ListRealtimeStreamsInfoResponse"];
+            }
+
+        }
+}
