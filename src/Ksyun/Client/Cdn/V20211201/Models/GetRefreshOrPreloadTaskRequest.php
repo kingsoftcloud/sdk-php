2469504849364f0af84b0070020ded60442d1de7
fengyikai <fengyikai@kingsoft.com>
2026-02-03 15:45:03 +0800

    内容分发网络(cdn)：更新GetRefreshOrPreloadTask参数

--- src/Ksyun/Client/Cdn/V20211201/Models/GetRefreshOrPreloadTaskRequest.php
+++ src/Ksyun/Client/Cdn/V20211201/Models/GetRefreshOrPreloadTaskRequest.php
@@ -8,9 +8,23 @@ class GetRefreshOrPreloadTaskRequest extends BaseModel
 {
     public $RequestParams = [
          /**String**/
-        "Action" => null,
+        "StartTime" => null,
          /**String**/
-        "Version" => null,
+        "EndTime" => null,
+         /**String**/
+        "TaskId" => null,
+         /**String**/
+        "DomainName" => null,
+         /**Array**/
+        "Urls" => null,
+         /**String**/
+        "Type" => null,
+         /**String**/
+        "SubType" => null,
+         /**Long**/
+        "PageSize" => null,
+         /**Long**/
+        "PageNumber" => null,
     ];
 
 
@@ -24,18 +38,67 @@ class GetRefreshOrPreloadTaskRequest extends BaseModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("Action",$param) and $param["Action"] !== null) {
-            if(is_bool($param["Action"])){
-                $this->RequestParams["Action"] = $param["Action"] ? "true" : "false";
+        if (array_key_exists("StartTime",$param) and $param["StartTime"] !== null) {
+            if(is_bool($param["StartTime"])){
+                $this->RequestParams["StartTime"] = $param["StartTime"] ? "true" : "false";
             } else {
-                $this->RequestParams["Action"] = $param["Action"];
+                $this->RequestParams["StartTime"] = $param["StartTime"];
             }
         }
-        if (array_key_exists("Version",$param) and $param["Version"] !== null) {
-            if(is_bool($param["Version"])){
-                $this->RequestParams["Version"] = $param["Version"] ? "true" : "false";
+        if (array_key_exists("EndTime",$param) and $param["EndTime"] !== null) {
+            if(is_bool($param["EndTime"])){
+                $this->RequestParams["EndTime"] = $param["EndTime"] ? "true" : "false";
             } else {
-                $this->RequestParams["Version"] = $param["Version"];
+                $this->RequestParams["EndTime"] = $param["EndTime"];
+            }
+        }
+        if (array_key_exists("TaskId",$param) and $param["TaskId"] !== null) {
+            if(is_bool($param["TaskId"])){
+                $this->RequestParams["TaskId"] = $param["TaskId"] ? "true" : "false";
+            } else {
+                $this->RequestParams["TaskId"] = $param["TaskId"];
+            }
+        }
+        if (array_key_exists("DomainName",$param) and $param["DomainName"] !== null) {
+            if(is_bool($param["DomainName"])){
+                $this->RequestParams["DomainName"] = $param["DomainName"] ? "true" : "false";
+            } else {
+                $this->RequestParams["DomainName"] = $param["DomainName"];
+            }
+        }
+        if (array_key_exists("Urls",$param) and $param["Urls"] !== null) {
+            if(is_bool($param["Urls"])){
+                $this->RequestParams["Urls"] = $param["Urls"] ? "true" : "false";
+            } else {
+                $this->RequestParams["Urls"] = $param["Urls"];
+            }
+        }
+        if (array_key_exists("Type",$param) and $param["Type"] !== null) {
+            if(is_bool($param["Type"])){
+                $this->RequestParams["Type"] = $param["Type"] ? "true" : "false";
+            } else {
+                $this->RequestParams["Type"] = $param["Type"];
+            }
+        }
+        if (array_key_exists("SubType",$param) and $param["SubType"] !== null) {
+            if(is_bool($param["SubType"])){
+                $this->RequestParams["SubType"] = $param["SubType"] ? "true" : "false";
+            } else {
+                $this->RequestParams["SubType"] = $param["SubType"];
+            }
+        }
+        if (array_key_exists("PageSize",$param) and $param["PageSize"] !== null) {
+            if(is_bool($param["PageSize"])){
+                $this->RequestParams["PageSize"] = $param["PageSize"] ? "true" : "false";
+            } else {
+                $this->RequestParams["PageSize"] = $param["PageSize"];
+            }
+        }
+        if (array_key_exists("PageNumber",$param) and $param["PageNumber"] !== null) {
+            if(is_bool($param["PageNumber"])){
+                $this->RequestParams["PageNumber"] = $param["PageNumber"] ? "true" : "false";
+            } else {
+                $this->RequestParams["PageNumber"] = $param["PageNumber"];
             }
         }
 
--- src/Ksyun/Client/Cdn/V20211201/Models/GetRefreshOrPreloadTaskResponse.php
+++ src/Ksyun/Client/Cdn/V20211201/Models/GetRefreshOrPreloadTaskResponse.php
@@ -5,8 +5,29 @@ use Ksyun\Common\BaseModel;
 
 class GetRefreshOrPreloadTaskResponse extends BaseModel
 {
+         /** 开始时间**/
+         public  $StartTime;
+
+         /** 结束时间**/
+         public  $EndTime;
+
+         /**Object 查询的URL列表**/
+         public  $Urls;
+
+         /** 整页大小**/
+         public  $PageSize;
+
+         /** 页码**/
+         public  $PageNumber;
+
+         /** Url总条数**/
+         public  $TotalCount;
+
+         /**Object 刷新或预热任务进度百分比及状态信息**/
+         public  $Datas;
+
          /** **/
-         public  $GetRefreshOrPreloadTaskResponse;
+         public  $Message;
 
          public function __construct()
          {
@@ -18,8 +39,29 @@ class GetRefreshOrPreloadTaskResponse extends BaseModel
             if ($param === null) {
                 return;
             }
-            if (array_key_exists("GetRefreshOrPreloadTaskResponse",$param) and $param["GetRefreshOrPreloadTaskResponse"] !== null) {
-                $this->GetRefreshOrPreloadTaskResponse = $param["GetRefreshOrPreloadTaskResponse"];
+            if (array_key_exists("StartTime",$param) and $param["StartTime"] !== null) {
+                $this->StartTime = $param["StartTime"];
+            }
+            if (array_key_exists("EndTime",$param) and $param["EndTime"] !== null) {
+                $this->EndTime = $param["EndTime"];
+            }
+            if (array_key_exists("Urls",$param) and $param["Urls"] !== null) {
+                $this->Urls = $param["Urls"];
+            }
+            if (array_key_exists("PageSize",$param) and $param["PageSize"] !== null) {
+                $this->PageSize = $param["PageSize"];
+            }
+            if (array_key_exists("PageNumber",$param) and $param["PageNumber"] !== null) {
+                $this->PageNumber = $param["PageNumber"];
+            }
+            if (array_key_exists("TotalCount",$param) and $param["TotalCount"] !== null) {
+                $this->TotalCount = $param["TotalCount"];
+            }
+            if (array_key_exists("Datas",$param) and $param["Datas"] !== null) {
+                $this->Datas = $param["Datas"];
+            }
+            if (array_key_exists("Message",$param) and $param["Message"] !== null) {
+                $this->Message = $param["Message"];
             }
 
         }
