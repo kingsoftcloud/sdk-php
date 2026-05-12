ccc911175f07671dc25e135a6b82aed66f25497f
fengyikai <fengyikai@kingsoft.com>
2026-05-12 19:22:03 +0800

    费用账单(bill-union)：实例按日汇总账单接口api更新

--- src/Ksyun/Client/Bill_union/V20221222/Models/QueryInstanceConsumeResponse.php
+++ src/Ksyun/Client/Bill_union/V20221222/Models/QueryInstanceConsumeResponse.php
@@ -6,21 +6,6 @@ use Ksyun\Common\BaseModel;
 class QueryInstanceConsumeResponse extends BaseModel
 {
          /** **/
-         public  $status;
-
-         /** 当前请求的RequestId**/
-         public  $requestId;
-
-         /** **/
-         public  $data;
-
-         /** **/
-         public  $error;
-
-         /** **/
-         public  $ok;
-
-         /** **/
          public  $Success;
 
          /** **/
@@ -38,21 +23,6 @@ class QueryInstanceConsumeResponse extends BaseModel
         {
             if ($param === null) {
                 return;
-            }
-            if (array_key_exists("status",$param) and $param["status"] !== null) {
-                $this->status = $param["status"];
-            }
-            if (array_key_exists("requestId",$param) and $param["requestId"] !== null) {
-                $this->requestId = $param["requestId"];
-            }
-            if (array_key_exists("data",$param) and $param["data"] !== null) {
-                $this->data = $param["data"];
-            }
-            if (array_key_exists("error",$param) and $param["error"] !== null) {
-                $this->error = $param["error"];
-            }
-            if (array_key_exists("ok",$param) and $param["ok"] !== null) {
-                $this->ok = $param["ok"];
             }
             if (array_key_exists("Success",$param) and $param["Success"] !== null) {
                 $this->Success = $param["Success"];
